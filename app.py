63e098608ebb8a0b008896b1e1d14e3601ada0cd
PhilosophyWithJosh <joshmendiola@icloud.com>
2024-12-08 17:50:56 -0600

    add CourseModels.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from flask import jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from pydantic import ValidationError
 import logging
-from CourseModels import Course, CourseStatus, CourseSection, CourseResponse, ErrorResponse
+from models.CourseModels import Course, CourseStatus, CourseSection, CourseResponse, ErrorResponse
 
 # Load environment variables
 load_dotenv()
